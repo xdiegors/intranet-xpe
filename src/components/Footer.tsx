c8d6f98de811848a33b541047bcbf0cca438c31f
xdiegors <xdiegors@outlook.com>
2023-09-22 13:52:43 -0300

    Added: Login/Logout button, Date order desc, Format Date on News.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,38 @@
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import LoginIcon from "@mui/icons-material/Login";
+import LogoutIcon from "@mui/icons-material/Logout";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+import AuthService from "../services/AuthService";
+import { useEffect, useState } from "react";
 
 export default function Footer() {
+  const navigate = useNavigate();
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Fetch the user's login status when the component mounts
+    const userToken = AuthService.getToken();
+    userToken !== null ? setIsUserLoggedIn(true) : setIsUserLoggedIn(false);
+  }, []); // Empty dependency array ensures this effect runs only once
+
+  const handleLogin = () => {
+    navigate("/login");
+  };
+
+  const handleLogout = () => {
+    AuthService.logout();
+    navigate("/");
+    setIsUserLoggedIn(false); // Update the login status
+  };
+
+  // Render null if the login status is still being determined
+  if (isUserLoggedIn === null) {
+    return null;
+  }
+
   return (
     <Box
       sx={{
@@ -12,10 +42,35 @@ export default function Footer() {
         alignItems: "center",
       }}
     >
-      <Container>
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
         <Typography variant="body2" color="white">
           &copy; DRS Móveis - 2023
         </Typography>
+        {isUserLoggedIn == true ? (
+          <Button
+            color="warning"
+            onClick={handleLogout}
+            endIcon={<LogoutIcon />}
+            variant="contained"
+          >
+            Sair
+          </Button>
+        ) : (
+          <Button
+            color="primary"
+            onClick={handleLogin}
+            endIcon={<LoginIcon />}
+            variant="contained"
+          >
+            Entrar
+          </Button>
+        )}
       </Container>
     </Box>
   );
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,15 +3,32 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
 import MenuItems from "./MenuItems";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function Menu() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/documents")
+      .then((response) => {
+        // Axios already parses JSON responses, so you can directly access the data property.
+
+        setData(response.data.files);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, [data]);
+
   return (
     <Box sx={{ flexGrow: 1 }} marginTop={1} marginBottom={1}>
       <AppBar position="static" sx={{ backgroundColor: "black" }}>
         <Toolbar>
           <Container sx={{ flexWrap: "wrap", flexDirection: "row" }}>
             <MenuItems title="Sobre a empresa" items={["Lojas", "História"]} />
-            <MenuItems title="Documentos" items={["teste", "teste 2"]} />
+            <MenuItems title="Documentos" items={data} />
             <MenuItems title="Procedimentos" items={["teste", "teste 2"]} />
             <MenuItems title="Formulários" items={["teste", "teste 2"]} />
           </Container>
